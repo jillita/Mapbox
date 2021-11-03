d0880ffb58ef8a08ca981cee50c42533e6dc0a05
jillita <85846898+jillita@users.noreply.github.com>
2021-11-03 18:20:20 -0400

    Add files via upload

---
+++ Denmark/config.js
@@ -0,0 +1,218 @@
+var config = {
+    style: 'mapbox://styles/jillita/ckc0w2abj3jx81iqrtgflkwtq',
+    accessToken: 'pk.eyJ1IjoiamlsbGl0YSIsImEiOiJjaWdoNXJ4bzM4NGNhdmdrcnczZmxnd2hyIn0.HA1T3_gMTk1ORJ9JUQ326w',
+    showMarkers: false,
+    markerColor: '#3FB1CE',
+    theme: 'dark',
+    use3dTerrain: true,
+    title: 'Holocaust Rescuers during World War II',
+    subtitle: 'Denmark and Knud Dyby',
+    byline: '',
+    footer: 'Source: source citations, etc.',
+    chapters: [
+//OPENING CHAPTER//
+        {
+            id: 'opening-chapter-id',
+            alignment: 'right',
+            hidden: false,
+            title: 'World War II Allies and Axis Countries in 1944',
+            image: './images/WWIICountriesLegend2b.png',
+            description: 'This map shows the extent of German occupation throughout Europe in April 1944.',
+                   location: {
+                //center: [16.84923, 48.09285],
+                //zoom: 4.09,
+center: [28.15015, 49.45338],
+zoom: 3.88,
+pitch: 0.00,
+bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+  
+            ],
+
+            onChapterExit: [
+
+            ],
+        },
+
+//FIRST CHAPTER//
+        {
+            id: 'chapter-one-id',
+            alignment: 'right',
+            hidden: false,
+            title: 'Chapter 1: The Danish Rescuers',
+            image: './images/KnudDyby1941.jpg',
+            description: 'One of the many remarkable rescuer stories is from Denmark. Surrounded by water and Germany to the south, the Danish Resistance helped Jews fleeing the country by shuttling them in fishing boats to nearby Sweden. When Yad Vashem approached each rescuer to award them with Righteous Among the Nations, they insited that they be recognized as a group, the only one to be done so.',
+            location: {
+                center: [16.93735, 55.53372],
+                zoom: 5.33,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo', 
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+      
+            ],
+            onChapterExit: [
+
+            ]
+        },
+		
+		//SECOND CHAPTER//
+        {
+            id: 'chapter-two-id',
+            alignment: 'full',
+            hidden: false,
+            title: 'Chapter 2: Growing up in Denmark',
+            image: '',
+            description: '<p align="center"><iframe width="100%" height="500" src="https://www.youtube.com/embed/IuJmz6rspTY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></p>',
+            location: {
+                center: [16.93735, 55.53372],
+                zoom: 5.33,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo', 
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+      
+            ],
+            onChapterExit: [
+
+            ]
+        },
+		
+				//SECOND B CHAPTER//
+        {
+            id: 'chapter-twob-id',
+            alignment: 'full',
+            hidden: false,
+            title: 'Chapter 2B: No Great Love for Germans',
+            image: '',
+            description: '<p align="center"><iframe width="100%" height="500" src="https://www.youtube.com/embed/d3rMZHCnhoE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></p>',
+            location: {
+                center: [16.93735, 55.53372],
+                zoom: 5.33,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo', 
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+      
+            ],
+            onChapterExit: [
+
+            ]
+        },
+
+//THIRD CHAPTER//
+        {
+            id: 'chapter-three-id',
+            alignment: 'right',
+            hidden: false,
+            title: 'Chapter 3: Boats from Copenhagen',
+            image: './images/RescuerDanishBoatSM.JPG',
+            description: 'Parents still had morals, tradition at time to behave to neighbors and self and family. 100 honest in everything, integrity. Goodness, always take care of old and sick in that society, in families or country. Neighbors helped around community too. ',
+            location: {
+                center: [12.91419, 55.55646],
+                zoom: 9.26,
+                pitch: 45,
+                bearing: 65
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+               
+            ],
+            onChapterExit: [
+
+            ]
+        },
+		
+		//FOURTH CHAPTER//
+        {
+            id: 'chapter-four-id',
+            alignment: 'full',
+            hidden: false,
+            title: 'Chapter 4: Night Ops',
+            image: '',
+            description: '<p align="center"><iframe width="100%" height="500" src="https://www.youtube.com/embed/LU4etqEoeZc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></p>',
+            location: {
+                center: [12.91419, 55.55646],
+                zoom: 9.26,
+                pitch: 45,
+                bearing: 65
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+               
+            ],
+            onChapterExit: [
+
+            ]
+        },
+		
+		//FIFTH CHAPTER//
+        {
+            id: 'chapter-five-id',
+            alignment: 'right',
+            hidden: false,
+            title: 'Chapter 5: Two weeks of evacuations',
+            image: './images/RescuerDanishBoatSM.JPG',
+            description: '<p>Participated as indv of Underground, told them what they knew in police dept, how to use weapons. Many underground groups, helped more by being independent. In 1943 joined a Danish Swedish underground group. Supported with money. Put their salaries into collective bank. </p> <p> Very few Danish Nazis, but the one he knew reported him. Owned bookshop across street. Sent letters to Nazi party and head of police dept to arrest him. happen. Shop keepers didnot want to sell to Germans, he would tell shops to hide their goods.</p><p> Freedom Fighters resistance group. Didn’t cooperate, too dangerous to know each other. Underground press service. Got reports from all over country, did help groups corroborate. 1944-45. Several hundred members in one group. Never overlapped.</p> ',
+            location: {
+center: [12.38415, 54.93759],
+zoom: 8.30,
+pitch: 43.00,
+bearing: 60.80
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+               
+            ],
+            onChapterExit: [
+
+            ]
+        },
+		
+				//SIXTH CHAPTER//
+        {
+            id: 'chapter-six-id',
+            alignment: 'right',
+            hidden: false,
+            title: 'Chapter 6: Denmark came together',
+            image: './images/RescuerDanishBoatSM.JPG',
+            description: 'They saved XXXXX by taking them to Sweden',
+            location: {
+                center: [16.93735, 55.53372],
+                zoom: 5.33,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+               
+            ],
+            onChapterExit: [
+
+            ]
+        },
+
+//MARKS BELOW ARE AT END//
+    ]
+};
